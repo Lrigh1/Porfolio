9f378b25e8d61de2440191d6784b9e41dbe66e63
CHIp Wright <frisbeeprod@msn.com>
2018-05-12 18:58:22 -0700

    further fixed styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,15 +14,16 @@ const AppHeader = () => (
                 <Image style={{marginTop: '-1.5%'}} src={profileImage} size='small' centered circular />
                 <h3 className='Text' style={{marginTop: '0%'}}>A developer from the great state of Arizona</h3>
                 <h4 className='Text' style={{marginTop: '-1%'}} >I enjoy sports, playing music, writing, tennis, trying to ride my motorcyle, playing games (Super Smash and Mario Kart!) chasing around my cats, and of course, coding!<br /></h4>  
-                <h4  style={{marginTop: '-1%', marginBottom: '-2%'}}>These websites are made using Reactv16, Redux, Node, Socket IO, Semantic UI, MongoDB, Mongoose, Express, and they incorporate es6 & es7 syntax.</h4>
+                <h4  style={{marginTop: '-1%', marginBottom: '-1%'}}>These websites are made using Reactv16, Redux, Node, Socket IO, Semantic UI, MongoDB, Mongoose, Express, and they incorporate es6 & es7 syntax.</h4>
           </Grid.Column>
         </Grid.Row>
         </Segment>
+        <Segment color="teal" style={{ width: '50%', marginTop: '0'}}>
         <Grid.Row centered columns={3} className='Display'>
             <Segment className='Display' style={{ background: '#D6E3E8B3'}} stacked>
                 <Grid.Column>
                 <h3>Websites:</h3>
-                <h5 style={{marginTop: '-3%'}}>(hosted on heroku, please allow their load)</h5>
+                <h5 style={{marginTop: '-4%'}}>(hosted on heroku, please allow their load)</h5>
                 <Page html="https://crystalclear.herokuapp.com/" title="Crystal Clear Pool Service" text={<div><br />A pool business website. <br />Clicking links will auto scroll you down.</div>}/>
 
                 <Page html="https://ricayustudios.herokuapp.com/" title="Ricayu's Photo Studio" text={<div><br />A website designed for a photographer's business</div>}/>
@@ -45,6 +46,7 @@ const AppHeader = () => (
                 </Grid.Column>
             </Segment>
         </Grid.Row>
+        </Segment>
     </Grid>
   </Header>
 )
--- src/components/Page.js
+++ src/components/Page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Segment } from 'semantic-ui-react'
 
 const Page = (props) => (
-    <Segment color='orange' >
+    <Segment classname='Display-Back' color='orange'>
         <Button color='teal' href={props.html} target="_blank">{props.title}</Button>
         <div>{props.text}</div>
     </Segment>
